c7108a426d611ed545fddf31ba490614835475df
Antonín <flowythe@gmail.com>
2024-02-21 10:53:04 +0100

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 7001;
+
+app.use(cors());
+
+var corsOptions = {
+	origin: ['https://flwn.dev', 'https://basil.florist'],
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 require('dotenv').config();
 
@@ -8,7 +16,7 @@ const method = 'user.getRecentTracks';
 const apiKey = process.env.API_KEY;
 const limit = '1';
 
-app.get('/', (req, res) => {
+app.get('/', cors(corsOptions), (req, res) => {
 	const user = 'flowyan';
 
 	const url = `http://ws.audioscrobbler.com/2.0/?method=${method}&api_key=${apiKey}&user=${user}&limit=${limit}&format=json`;
@@ -23,7 +31,7 @@ app.get('/', (req, res) => {
 		});
 });
 
-app.get('/chutoy', (req, res) => {
+app.get('/chutoy', cors(corsOptions), (req, res) => {
 	const user = 'chutoyjohn';
 
 	const url = `http://ws.audioscrobbler.com/2.0/?method=${method}&api_key=${apiKey}&user=${user}&limit=${limit}&format=json`;
